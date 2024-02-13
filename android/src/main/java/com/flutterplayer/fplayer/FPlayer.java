6592da026725b3fdcfbb31009e4a14ec2f2cb52a
max <veluxa@qq.com>
2024-02-13 19:07:54 +0800

    补充依赖

--- android/src/main/java/com/flutterplayer/fplayer/FPlayer.java
+++ android/src/main/java/com/flutterplayer/fplayer/FPlayer.java
@@ -42,7 +42,9 @@ import tv.danmaku.ijk.media.player.misc.IMediaDataSource;
 import tv.danmaku.ijk.media.player.misc.IjkTrackInfo;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
